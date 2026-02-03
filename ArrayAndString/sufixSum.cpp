769f5633dd8ed5846663cfc6d28fc29185038aec
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-03 18:58:07 +0600

    Function call to fill suffix sum array and Printing the computed suffix sum array.

--- ArrayAndString/sufixSum.cpp
+++ ArrayAndString/sufixSum.cpp
@@ -1,24 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> createSuffixSumArray(vector<int> arr, int n){
-    vector<int> suffixSum(n , 0);
+vector<int> createSuffixSumArray(vector<int> arr, int n)
+{
+    vector<int> suffixSum(n, 0);
 
     suffixSum[n - 1] = arr[n - 1];
 
-    for(int i = n - 2; i >= 0; i--){
+    for (int i = n - 2; i >= 0; i--)
+    {
         suffixSum[i] = suffixSum[i + 1] + arr[i];
     }
 
     return suffixSum;
 }
 
-int main(){
+int main()
+{
 
     vector<int> arr = {10, 15, 70, 82, 30, 40};
     int n = arr.size();
 
     vector<int> suffixSum = createSuffixSumArray(arr, n);
-    
+
+    cout << "Suffix sum array: ";
+    for (int i = 0; i < n; i++)
+        cout << suffixSum[i] << " ";
     return 0;
 }
