721de6cac1f0fe78c363c2d0897df1bf0fde7f24
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-09 22:12:41 +0600

    call binarySearch() function in main function for finding index.

--- Searching/Binary_Search.cpp
+++ Searching/Binary_Search.cpp
@@ -1,28 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int>& arr, int target){
+int binarySearch(vector<int> &arr, int target)
+{
     int left = 0;
     int right = arr.size() - 1;
 
-    while(left <= right){
+    while (left <= right)
+    {
         int mid = left + (right - left) / 2;
 
-        if(arr[mid] == target){
+        if (arr[mid] == target)
+        {
             return mid;
         }
 
-        else if(arr[mid] < target){
+        else if (arr[mid] < target)
+        {
             left = mid + 1;
         }
 
-        else{
+        else
+        {
             right = mid - 1;
         }
     }
     return -1;
 }
 
-int main(){
-     vector<int> arr = {2, 5, 8, 12, 16, 23, 38, 45, 56, 67, 78};
+int main()
+{
+    vector<int> arr = {2, 5, 8, 12, 16, 23, 38, 45, 56, 67, 78};
+
+    cout << "Sorted Array: ";
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+
+    cout << "\n";
+
+    vector<int> arr2 = {23, 2, 78, 100, 12};
+
+    for (int target2 : arr2)
+    {
+        int index = binarySearch(arr2, target2);
+        if (index != -1)
+        {
+            cout << "Element " << target2 << " found at index " << index << "\n";
+        }
+        else
+        {
+            cout << "Element " << target2 << " not found in array\n";
+        }
+    }
+
+    return 0;
 }
