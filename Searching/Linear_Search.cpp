4ee782737096ee35f4b12f8c72375ac3ad05d674
fazlerabbi13 <mohammad0132794@gmail.com>
2026-02-10 21:54:15 +0600

    call search() function in main function.

--- Searching/Linear_Search.cpp
+++ Searching/Linear_Search.cpp
@@ -1,13 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int search(vector<int>& arr, int x){
-    for(int i = 0; i < arr.size(); i++){
-        if(arr[i] == x){
+int search(vector<int> &arr, int x)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == x)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int 
+int main()
+{
+    vector<int> arr = {1, 5, 8, 3, 9, 7};
+
+    int x = 0;
+
+    int result = search(arr, x);
+
+    if (result == -1)
+    {
+        cout << "Element is not present in the array" << "\n";
+    }
+    else
+    {
+        cout << "Element is present in the array" << "\n";
+    }
+    
+    return 0;
+}
